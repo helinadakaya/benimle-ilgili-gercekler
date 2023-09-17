57c9473eb33c6533c03fc0f32d852cc142e0136a
helinada kaya <145283091+helinadakaya@users.noreply.github.com>
2023-09-17 19:59:00 +0300

    Create main.py
    phyton dosyası eklendi

---
+++ main.py
@@ -0,0 +1,2 @@
+import random 
+benim_hakkimda_bazı_seyler =["en sevdiğim hobim resim yapmak"",bir kuşum var""en gurur duyduğum projem bu sene teknofest için yaptığımız proje fakat sınıvları kaçırdığım için gidememiştim,en sevdiğim yemek mantı"]
